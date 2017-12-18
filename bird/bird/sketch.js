9ec7e4542383b2ad7909e86aa7486ee4338225c6
nilan10 <34275146+nilan10@users.noreply.github.com>
2017-12-18 12:22:22 -0500

    Add files via upload

---
+++ bird/bird/sketch.js
@@ -0,0 +1,167 @@
+// names variables
+var bird;
+var pipes = [];
+//it sets up the bacic rules of the code
+function setup() {
+  createCanvas(400, 600);
+  // says to make a new Bird Function every time bird is present
+  bird = new Bird();
+ 
+  pipes.push(new Pipe());
+  
+  new Audio('https://streams.my-free-mp3.net/GvWBZB:mHx1rB','_blank').play()
+
+}
+//this is what wil be drawn on the screen
+function draw() {
+  background(10,0,230);
+//this contols the game score at the top
+   
+  fill(255,255,255)
+  textSize(32) 
+ text(frameCount,100,100,200);
+  
+  fill(200,200,0)
+  ellipse(50,50,100,100)
+  
+  fill(150,100,100)
+ rect(100,200,50,400)
+  rect(300,400,50,200)
+  
+  fill(0,200,0);
+  rect(50,200,150,140)
+  ellipse(0,600,100,200)
+ ellipse(100,600,100,200)
+   ellipse(200,600,100,200)
+   ellipse(300,600,100,200)
+   ellipse(400,600,100,200)
+  ellipse(325,350,100,150)
+  
+  
+  
+
+  for (var i = pipes.length-1; i >= 0; i--) {
+    pipes[i].show();
+    pipes[i].update();
+
+    if (pipes[i].hits(bird)) {
+      console.log("HIT");
+    }
+
+
+    if (pipes[i].offscreen()) {
+      pipes.splice(i, 1);
+    }
+  
+
+  }
+
+  bird.update();
+  bird.show();
+  if (frameCount % 100 == 0) {
+    pipes.push(new Pipe());
+  }
+
+
+}
+function mousePressed() {
+    bird.up();
+    //console.log("SPACE");
+  }
+ 
+
+function Bird() {
+  this.y = height/2;
+  this.x = 64;
+
+  this.gravity = 0.6;
+  this.lift = -15;
+  this.velocity = 0;
+
+  this.show = function() {
+    fill(255,255,0);
+    ellipse(this.x, this.y, 65, 32);
+    fill(255)
+    ellipse (this.x,this.y-10,10,10)
+    line(this.x+15,this.y, this.x+40, this.y)
+    fill(0)
+    ellipse(this.x, this.y-10,5,5)
+    fill(255,100,0)
+    triangle(this.x+15,this.y-15,this.x+15,this.y+15, this.x+40, this.y) 
+      fill(0,255,0)
+     
+  triangle(this.x+10,this.y,this.x-25, this.y,this.x-25,this.y+20)
+    
+   
+  }
+  {this.x+10,this.y,this.x-25, this.y,this.x-25,this.y+20}
+  this.up = function() {
+    this.velocity += this.lift;
+  
+  }
+
+  this.update = function() {
+    this.velocity += this.gravity;
+    this.velocity *= 0.9;
+    this.y += this.velocity;
+
+    if (this.y > height) {
+      this.y = height;
+      this.velocity = 0;
+    }
+
+    if (this.y < 0) {
+      this.y = 0;
+      this.velocity = 0;
+    }
+  }
+}
+
+function Pipe() {
+  this.top = random(height/2);
+  this.bottom = random(height/2);
+  this.x = width;
+  this.w = 20;
+  this.speed = 2;
+
+  this.highlight = false;
+
+  this.hits = function(bird) {
+    if (bird.y < this.top || bird.y > height - this.bottom) {
+      if (bird.x > this.x && bird.x < this.x + this.w) {
+        this.highlight = true;
+        return true;
+      }
+    }
+    this.highlight = false;
+    return false;
+  }
+
+  this.show = function() {
+    fill(255);
+    if (this.highlight) {
+      fill(255, 0, 0);
+      textSize(32);
+      text("Game Over", 200, 200);
+    noLoop()
+      
+    
+      
+    }
+    rect(this.x, 0, this.w, this.top);
+    rect(this.x, height-this.bottom, this.w, this.bottom);
+  }
+
+  this.update = function() {
+    this.x -= this.speed;
+  }
+
+  this.offscreen = function() {
+    if (this.x < -this.w) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+}
